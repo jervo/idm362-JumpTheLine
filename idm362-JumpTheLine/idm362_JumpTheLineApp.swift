118f612ed7bcb6fc2822c0548291085e49b0cfcb
Jervis Thompson <jervo@mac.com>
2025-01-14 19:11:50 -0500

    Initial Commit

---
+++ idm362-JumpTheLine/idm362_JumpTheLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  idm362_JumpTheLineApp.swift
+//  idm362-JumpTheLine
+//
+//  Created by Jervis Thompson on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct idm362_JumpTheLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
